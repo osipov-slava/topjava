9bc1b6bb6a1360c2e8f6298395fb50f4c97208a0
Slava Osipov <skybright@mail.ru>
2020-02-12 21:42:12 +0300

    add Meal & MealTo

---
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -0,0 +1,46 @@
+package ru.javawebinar.topjava.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Meal {
+    private int id;
+
+    private final LocalDateTime dateTime;
+
+    private final String description;
+
+    private final int calories;
+
+    public Meal(int id, LocalDateTime dateTime, String description, int calories) {
+        this.id = id;
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public LocalDate getDate() {
+        return dateTime.toLocalDate();
+    }
+
+    public LocalTime getTime() {
+        return dateTime.toLocalTime();
+    }
+}
